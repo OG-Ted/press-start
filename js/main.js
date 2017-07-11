173bd874b266eda4001072a4a9d87219c9395f5c
peterlaxalt <pete@laxaltandmciver.co>
2017-07-11 00:58:25 -0700

    nav buttons + nav consciousness

--- js/main.js
+++ js/main.js
@@ -9,30 +9,55 @@ $(document).ready(function() {
       $("#active").css({
         "opacity": "0"
       });
+      $("#nav-left").addClass('nudge');
+      $("#nav-right").removeClass('nudge');
+      $("#nav-top").removeClass('nudge');
+      $("#nav-bottom").removeClass('nudge');
+      $("#nav-home").removeClass('nudge');
     }
     function rightArrowPressed() {
       window.location = '#left';
       $("#active").css({
         "opacity": "0"
       });
+      $("#nav-right").addClass('nudge');
+      $("#nav-left").removeClass('nudge');
+      $("#nav-top").removeClass('nudge');
+      $("#nav-bottom").removeClass('nudge');
+      $("#nav-home").removeClass('nudge');
     }
     function upArrowPressed() {
       window.location = '#bottom';
       $("#active").css({
         "opacity": "0"
       });
+      $("#nav-top").addClass('nudge');
+      $("#nav-left").removeClass('nudge');
+      $("#nav-right").removeClass('nudge');
+      $("#nav-bottom").removeClass('nudge');
+      $("#nav-home").removeClass('nudge');
     }
     function downArrowPressed() {
       window.location = '#top';
       $("#active").css({
         "opacity": "0"
       });
+      $("#nav-bottom").addClass('nudge');
+      $("#nav-left").removeClass('nudge');
+      $("#nav-right").removeClass('nudge');
+      $("#nav-top").removeClass('nudge');
+      $("#nav-home").removeClass('nudge');
     }
     function spaceBarPressed() {
       window.location = '#home';
       $("#active").css({
         "opacity": "1"
       });
+      $("#nav-home").addClass('nudge');
+      $("#nav-left").removeClass('nudge');
+      $("#nav-right").removeClass('nudge');
+      $("#nav-top").removeClass('nudge');
+      $("#nav-bottom").removeClass('nudge');
     }
 
     document.onkeydown = function(evt) {
