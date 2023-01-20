abeef210f87e4576caeceedb1d14442accef8c60
Jose Rios <jose@commence.studio>
2023-01-19 23:30:32 -0800

    :rocket: Test generate file

--- docs/generate.js
+++ docs/generate.js
@@ -1,5 +1,5 @@
-// const core = require('@actions/core')
-// const github =require ('@actions/github')
+const core = require('@actions/core')
+const github =require ('@actions/github')
 
 // NodeJS-based GROQ queries that are persisted into the filesystem
 const sanityClient = require("@sanity/client")
